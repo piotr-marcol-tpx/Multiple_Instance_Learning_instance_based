Fix in data provision and (I guess?) encoder - reverse gradient layer was not operating on embeddings (for embedding_bool=True)

train/data_provision_tools.py
@@ -14,6 +14,7 @@ class HDF5Dataset(data.Dataset):
             images_dataset_name="patches",
             domain_gt_dataset_name="domain_oh",
             tissue_gt_dataset_name="tissue_oh",
+            approach="multiclass"
     ):
         self.file_path = file_path
         self.mode = mode
@@ -21,6 +22,7 @@ class HDF5Dataset(data.Dataset):
         self.domain_gt_dataset_name = domain_gt_dataset_name
         self.tissue_gt_dataset_name = tissue_gt_dataset_name
         self.length = None
+        self.approach = approach
         self._open_hdf5()
 
         with h5py.File(self.file_path, 'r') as hf:
@@ -71,7 +73,10 @@ class HDF5Dataset(data.Dataset):
         # h_e_matrix = torch.FloatTensor(h_e_matrix)
 
         # return k, q, h_e_matrix, domain_oh
-        return k, q, domain_oh
+        if self.approach == "multiclass":
+            return k, q, domain_oh
+        elif self.approach == "binary":
+            return k, q, domain_oh[-1:]
 
 
 class BalancedMultimodalSampler(torch.utils.data.sampler.Sampler):

train/models.py
@@ -107,25 +107,29 @@ class Encoder(torch.nn.Module):
         if self.embedding_bool:
             embedding_layer = self.embedding(conv_layers_out)
             embedding_layer = self.prelu(embedding_layer)
-
             features_to_return = embedding_layer
+            normalized_output = torch.nn.functional.normalize(features_to_return, dim=1)
+
+            if mode == "train":
+                reverse_feature = ReverseLayerF.apply(embedding_layer, alpha)
+                output_domain = self.domain_predictor(reverse_feature)
+                return normalized_output, output_domain
+
+            return normalized_output
 
         else:
             features_to_return = conv_layers_out
+            normalized_output = torch.nn.functional.normalize(features_to_return, dim=1)
 
-        normalized_array = torch.nn.functional.normalize(features_to_return, dim=1)
+            if mode == "train":
+                reverse_feature = ReverseLayerF.apply(features_to_return, alpha)
+                output_domain = self.domain_predictor(reverse_feature)
+                return normalized_output, output_domain
 
-        if mode == 'train':
-            reverse_feature = ReverseLayerF.apply(conv_layers_out, alpha)
-
-            output_domain = self.domain_predictor(reverse_feature)
-
-            return normalized_array, output_domain
-
-        return normalized_array
+            return normalized_output
 
 
-#reverse autograd
+# reverse autograd
 class ReverseLayerF(torch.autograd.Function):
 
     @staticmethod