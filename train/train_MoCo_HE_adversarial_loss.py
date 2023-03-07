95522046fb95cbea7a27d91813a14af2973c0313
Piotr Marcol <piotr.marcol@tooploox.com>
2023-03-07 08:37:34 +0100

    do not include domain loss in global loss - experiment for comparison

--- train/train_MoCo_HE_adversarial_loss.py
+++ train/train_MoCo_HE_adversarial_loss.py
@@ -392,8 +392,8 @@ while epoch < num_epochs and early_stop_cont < EARLY_STOP_NUM:
 		# loss_domains = lambda_val * criterion_domain(he_q, he_staining)
 		loss_domains = lambda_val * criterion_domain(he_q, domain_oh)
 
-		loss = loss_moco + loss_domains
-
+		# loss = loss_moco + loss_domains
+		loss = loss_moco
 		loss.backward()
 
 		# Encoder update
